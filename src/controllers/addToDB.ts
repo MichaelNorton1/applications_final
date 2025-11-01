30e558f5bd0f6123ea324f1394de9c104c5f33ae
michael <michael.norton1213@gmail.com>
2025-11-01 11:08:22 -0400

    db

--- src/controllers/addToDB.ts
+++ src/controllers/addToDB.ts
@@ -1,12 +1,26 @@
 import {neon} from '@neondatabase/serverless';
-
+import {Request, Response} from "express";
 const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
 
 const sql = neon(`postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`);
 
-export async function getPgVersion() {
-    const result = await sql`SELECT version()`;
-    console.log(result[0]);
+export async function getPgVersion( req: Request, res: Response ) {
+
+
+try {
+
+    const getTop5= await fetch('/api/nfl')
+
+    const top5 = await getTop5.json();
+
+    return res.status(200).send(top5)
+}catch (e) {
+    console.error(e);
+
+}
+
+
+
 }
 
 
